0a9a17560d839d0bbeafa47e4a0cb305004f266a
Azim Kutrev <azimkutrev9@gmail.com>
2026-07-01 12:16:48 +0300

    Fetch user data from API

---
+++ api_test.py
@@ -0,0 +1,11 @@
+import requests
+
+url = 'https://jsonplaceholder.typicode.com/users'
+response = requests.get(url)
+
+# Проверяваме дали сървърът ни е отговорил с 200 OK (Успех)
+if response.status_code == 200:
+    data = response.json()
+    print(data[0]["name"])
+else:
+    print(f"Грешка при връзка със сървъра. Код: {response.status_code}")gi
